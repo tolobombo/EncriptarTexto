e068d3167710d71a7c1e2195ae9563794f1864e6
Fernando Reyes <hapoweiser@gmail.com>
2022-03-07 09:38:30 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace EncriptarTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(88, 21);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(200, 26);
+            this.txt1.TabIndex = 0;
+            // 
+            // txt2
+            // 
+            this.txt2.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(88, 149);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(200, 26);
+            this.txt2.TabIndex = 1;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(88, 79);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(100, 35);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "Encriptar";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(188, 79);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(100, 35);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "Desencriptar";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(148, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Limpiar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Entrada:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(25, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Salida:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 311);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Encriptar/Desencriptar Texto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncriptarTexto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        //almaceno el texto en una variable para separar cada caracter
+        //y por medio de un For envio letra por letra para sustituirla por otra
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            output = "";
+
+            string input = txt1.Text;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                encriptar(input[i].ToString());
+            }
+
+            txt2.Text = output;
+            //txt1.Text = "";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            output = "";
+
+            string input = txt1.Text;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                desencriptar(input[i].ToString());
+            }
+
+            txt2.Text = output;
+            //txt1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "";
+            txt2.Text = "";
+            output = "";
+        }
+
+        public string output = "";
+        public void encriptar(string letra)
+        {
+            //por cada letra que va recibiendo la compara y la intercambia por la letra asignada
+            //concatenando cada letra en un string para devolver el texto junto.
+
+            if (letra == "A")
+            {
+                output = output + "K";
+            }
+            if (letra == "B")
+            {
+                output = output + "D";
+            }
+            if (letra == "C")
+            {
+                output = output + "F";
+            }
+            if (letra == "D")
+            {
+                output = output + "J";
+            }
+            if (letra == "E")
+            {
+                output = output + "N";
+            }
+            if (letra == "F")
+            {
+                output = output + "R";
+            }
+            if (letra == "G")
+            {
+                output = output + "B";
+            }
+            if (letra == "H")
+            {
+                output = output + "C";
+            }
+            if (letra == "I")
+            {
+                output = output + "G";
+            }
+            if (letra == "J")
+            {
+                output = output + "L";
+            }
+            if (letra == "K")
+            {
+                output = output + "X";
+            }
+            if (letra == "L")
+            {
+                output = output + "Z";
+            }
+            if (letra == "M")
+            {
+                output = output + "Y";
+            }
+            if (letra == "N")
+            {
+                output = output + "A";
+            }
+            if (letra == "Ñ")
+            {
+                output = output + "V";
+            }
+            if (letra == "O")
+            {
+                output = output + "Q";
+            }
+            if (letra == "P")
+            {
+                output = output + "I";
+            }
+            if (letra == "Q")
+            {
+                output = output + "E";
+            }
+            if (letra == "R")
+            {
+                output = output + "M";
+            }
+            if (letra == "S")
+            {
+                output = output + "O";
+            }
+            if (letra == "T")
+            {
+                output = output + "P";
+            }
+            if (letra == "U")
+            {
+                output = output + "T";
+            }
+            if (letra == "V")
+            {
+                output = output + "W";
+            }
+            if (letra == "W")
+            {
+                output = output + "U";
+            }
+            if (letra == "X")
+            {
+                output = output + "Ñ";
+            }
+            if (letra == "Y")
+            {
+                output = output + "H";
+            }
+            if (letra == "Z")
+            {
+                output = output + "S";
+            }
+        }
+
+        public void desencriptar(string letra)
+        {
+            if (letra == "K")
+            {
+                output = output + "A";
+            }
+            if (letra == "D")
+            {
+                output = output + "B";
+            }
+            if (letra == "F")
+            {
+                output = output + "C";
+            }
+            if (letra == "J")
+            {
+                output = output + "D";
+            }
+            if (letra == "N")
+            {
+                output = output + "E";
+            }
+            if (letra == "R")
+            {
+                output = output + "F";
+            }
+            if (letra == "B")
+            {
+                output = output + "G";
+            }
+            if (letra == "C")
+            {
+                output = output + "H";
+            }
+            if (letra == "G")
+            {
+                output = output + "I";
+            }
+            if (letra == "L")
+            {
+                output = output + "J";
+            }
+            if (letra == "X")
+            {
+                output = output + "K";
+            }
+            if (letra == "Z")
+            {
+                output = output + "L";
+            }
+            if (letra == "Y")
+            {
+                output = output + "M";
+            }
+            if (letra == "A")
+            {
+                output = output + "N";
+            }
+            if (letra == "V")
+            {
+                output = output + "Ñ";
+            }
+            if (letra == "Q")
+            {
+                output = output + "O";
+            }
+            if (letra == "I")
+            {
+                output = output + "P";
+            }
+            if (letra == "E")
+            {
+                output = output + "Q";
+            }
+            if (letra == "M")
+            {
+                output = output + "R";
+            }
+            if (letra == "O")
+            {
+                output = output + "S";
+            }
+            if (letra == "P")
+            {
+                output = output + "T";
+            }
+            if (letra == "T")
+            {
+                output = output + "U";
+            }
+            if (letra == "W")
+            {
+                output = output + "V";
+            }
+            if (letra == "U")
+            {
+                output = output + "W";
+            }
+            if (letra == "Ñ")
+            {
+                output = output + "X";
+            }
+            if (letra == "H")
+            {
+                output = output + "Y";
+            }
+            if (letra == "S")
+            {
+                output = output + "Z";
+            }
+        }
+
+        
+    }
+}
